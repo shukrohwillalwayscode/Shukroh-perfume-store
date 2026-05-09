88325d6bd3b7b377bb6f04b06d26767e92081bc4
shukrohwillalwayscode <abdurraheemshukroh2018@gmail.com>
2026-05-09 10:43:36 +0100

    fix

--- src/mockData/perfumes.ts
+++ src/mockData/perfumes.ts
@@ -3,47 +3,41 @@ import type { PerfumeTypes } from "../types/types";
 export const PERFUMES: PerfumeTypes[] = [
   {
     title: "Perf A",
-    description: "Sweet Smell",
+    description: "MY lOVE",
     img: "/images/product1.jpg",
-    price: "200",
+    price: "5000",
   },
 
-  // {
-  //   title: "Perf B",
-  //   description: "Sweet Smell",
-  //   img: "/public/images/product2.jpg",
-  //   price: "200",
-  // },
+  {
+    title: "Perf B",
+    description: "YARA",
+    img: "/images/product3.jpg",
+    price: "7000",
+  },
   {
     title: "Perf C",
-    description: "Sweet Smell",
-    img: "/images/product3.jpg",
-    price: "200",
+    description: "KHAMRAH",
+    img: "/images/product4.jpg",
+    price: "17000",
   },
   {
     title: "Perf D",
-    description: "Sweet Smell",
-    img: "/images/product4.jpg",
-    price: "200",
-  },
-  {
-    title: "Perf E",
-    description: "Sweet Smell",
+    description: "OUD AL LAYL",
     img: "/images/product5.jpg",
-    price: "200",
+    price: "11000",
   },
 
   {
-    title: "Perf F",
-    description: "Sweet Smell",
+    title: "Perf E",
+    description: "RIGSS",
     img: "/images/product6.jpg",
     price: "200",
   },
 
   {
-    title: "Perf G",
-    description: "Sweet Smell",
+    title: "Perf F",
+    description: "KARIS",
     img: "/images/product7.jpg",
-    price: "200",
+    price: "4000",
   },
 ];
